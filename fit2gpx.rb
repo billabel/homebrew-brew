60bec43fb2d93f96ea825dedb6dff3a2d28cd892
Bill Abel <billabel@me.com>
2019-03-20 14:43:04 -0500

    Changed the github URL to use raw=true

--- fit2gpx.rb
+++ fit2gpx.rb
@@ -1,7 +1,7 @@
 class Fit2gpx < Formula
   desc "A simple command line tool for converting Garmin FIT files to GPX files."
   homepage "https://github.com/billabel/fit2gpx"
-  url "https://github.com/billabel/fit2gpx/blob/master/archive/fit2gpx-0.0.1.tar.gz"
+  url "https://github.com/billabel/fit2gpx/blob/master/archive/fit2gpx-0.0.1.tar.gz?raw=true"
   sha256 "449ba11f89093e5222fb29c59273837e16754f088064712b53a766eeea523cff"
   version "0.0.1"
 
