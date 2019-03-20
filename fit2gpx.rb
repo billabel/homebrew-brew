afa224bc24a99a2fdd61ec278caf06f537147606
Bill Abel <billabel@me.com>
2019-03-20 14:32:26 -0500

    Updated sha for the tarball

--- fit2gpx.rb
+++ fit2gpx.rb
@@ -2,7 +2,7 @@ class Fit2gpx < Formula
   desc "A simple command line tool for converting Garmin FIT files to GPX files."
   homepage "https://github.com/billabel/fit2gpx"
   url "https://github.com/billabel/fit2gpx/blob/master/archive/fit2gpx-0.0.1.tar.gz"
-  sha256 "80b0ffc28c5bffd0bdc6579b1dc3f83ba9d730379d80e464aebc33ecbdf47823"
+  sha256 "6e838a2378f9ac7c4972fc9fb0afbd753dec3dcb825fd4381913d7b90ae01533"
   version "0.0.1"
 
   depends_on "gpsbabel" => :build
